1a74b28ca04754f53868b8f87917e6262c199db3
Abhishek Upperwal <a.upperwal@gmail.com>
2018-04-27 12:33:12 +0530

    Custom program name

--- xgdb.go
+++ xgdb.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"flag"
+	"os"
+
 	"github.com/upperwal/xgdb/gdb"
 )
 func main() {
 
-	gdb, err := gdb.NewGDB("main")
+	progName := flag.String("attach", "", "Program name to attach xGDB")
+	flag.Parse()
+
+	if *progName == "" {
+		fmt.Println("Give a program name to attach gdb")
+		fmt.Println("Ex: xgdb -attach main")
+		os.Exit(1)
+	}
+
+	gdb, err := gdb.NewGDB(*progName)
 	if err != nil {
 		panic(err)
 	}
